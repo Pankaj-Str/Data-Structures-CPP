19c10723f03262d3d604627e69e76841c0c8b9c9
Pankaj Chouhan <pankajchouhan80@gmail.com>
2023-11-26 16:02:14 +0530

    Selection Sort

---
+++ Sorting techniques/SelectionSort.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+void selectionSort(int arr[], int n) {
+    for (int i = 0; i < n - 1; ++i) {
+        int min_index = i;
+        for (int j = i + 1; j < n; ++j) {
+            if (arr[j] < arr[min_index]) {
+                min_index = j;
+            }
+        }
+        // Swap the found minimum element with the first element
+        std::swap(arr[i], arr[min_index]);
+    }
+}
+
+int main() {
+    int arr[] = {64, 25, 12, 22, 11};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    selectionSort(arr, n);
+
+    std::cout << "Sorted array: ";
+    for (int i = 0; i < n; ++i) {
+        std::cout << arr[i] << " ";
+    }
+
+    return 0;
+}
